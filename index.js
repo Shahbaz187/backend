0ef9a91d7d9c9f795ceb77c0bfb4c87d9e7d931f
shahbazansari8199@gmail.com <shahbazansari8199@gmail.com>
2024-05-20 21:10:40 +0500

    new server

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-import routes from './routes/passroutes.js'
+import { passModel } from './models/passmodel.js'
 
 dotenv.config()
 await mongoose.connect(process.env.MONGOURL)
@@ -14,12 +14,23 @@ const port = process.env.PORT || 5000
 app.use(bodyParser.json())
 app.use(cors())
 
-app.use('/api' , routes )
-
-app.get('/' , (req , res) =>{
-    res.json({message: "Hello world"})
+app.post('/' , async(req , res) =>{
+    try {
+        const passDB = new passModel(req.body)
+        const savedData = await passDB.save()
+        res.send(savedData)
+    } catch (error) {
+        console.log(error);
+    }
 })
-
+app.get('/' , async(req , res) =>{
+    try {
+        const allData = await passModel.find({})
+        res.json(allData)
+    } catch (error) {
+        console.log(error);
+    }
+})
 app.listen(port , () =>{
     console.log(`your server is runing on ${port}`);
 })
--- routes/passroutes.js
+++
@@ -1,8 +0,0 @@
-import { create} from "../controller/passcontroller.js"
-import express from 'express'
-
-const routes = express.Router()
-
-routes.post('/create', create )
-
-export default routes
